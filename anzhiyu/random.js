846fb4ac83a549a65bcbdb47be4ef88aa25858f7
sl818 <sl@sl00.top>
2023-04-05 02:55:55 +0800

    Site updated: 2023-04-05 02:55:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/03/hello-world/","2023/04/03/第一篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/04/03/hello-world/","2023/04/03/第一篇文章/","2023/04/05/搭建Hexo并托管到Github/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
